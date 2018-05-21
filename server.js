11f2eed5138bd79a7597adcd638d083987c6afe7
Louie Tse <lttse@myseneca.ca>
2018-05-21 17:02:07 -0400

    dept

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
+var dataservice = require("dataservice.js");
 var app = express();
 
 app.use(express.static('public'));
 // Routes
 
-//response.writeHead(200, {'Content-Type': 'text/html'});
-
 app.get("/", (req, res) => {
     res.sendFile('views/home.html' , { root : __dirname});
 });
-
 app.get("/home", (req, res) => {
     res.sendFile('views/home.html' , { root : __dirname});
 });
 app.get("/about", (req, res) => {
     res.sendFile('views/about.html' , { root : __dirname});
 });
+app.get("/employees", (req, res) => {
+    res.send("test1 works");
+});
+app.get("/managers", (req, res) => {
+    res.send("test2 works");
+});
+app.get("/departments", (req, res) => {
+    res.send("test3 works");
+});
+
+//res.status(404).send("Page Not Found");
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT);
