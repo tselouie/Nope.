57ab19c55593018b6b85cfb5f07457ecfcd852dd
Louie Tse <lttse@myseneca.ca>
2018-05-21 16:11:49 -0400

    f

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var app = express();
 app.use(express.static('public'));
 // Routes
 
-app.get("/", (req, res) => {
-    res.send("web322-app/views/home.html");
+app.get("/home", (req, res) => {
+    res.sendFile('web322-app/views/home.html');
 });
 app.get("/about", (req, res) => {
-    res.sendFile("web322-app/views/about.html");
+    res.sendFile('about.html');
 });
 
 // setup http server to listen on HTTP_PORT
